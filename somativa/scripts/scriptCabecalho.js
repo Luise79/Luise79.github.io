b1f7f2c890c73ae2a32362af317e893b441b2757
Luise <106396320+Luise79@users.noreply.github.com>
2025-06-15 21:32:51 +0000

    continuando

--- somativa/scripts/scriptCabecalho.js
+++ somativa/scripts/scriptCabecalho.js
@@ -1,5 +1,4 @@
 document.getElementById("login").addEventListener("click", function() {
-    console.log("deu");
     document.getElementById("loginTab").style.display = "block";
 });
 
@@ -7,6 +6,7 @@ document.getElementById("closetab").addEventListener("click", function() {
     document.getElementById("loginTab").style.display = "none";
 });
 
-document.getElementById("buttonLogin").addEventListener("submit", function() {
+document.getElementById("buttonLogin").addEventListener("click", function() {
+    console.log("deu");
     alert("Não é possível fazer o login no momento. Por favor, volte mais tarde!");
 })

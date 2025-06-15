03cc7d094bbf9cae26dc70f468fe825f418fc42c
Luise <106396320+Luise79@users.noreply.github.com>
2025-06-15 20:07:04 +0000

    styling login

--- somativa/scripts/scriptCabecalho.js
+++ somativa/scripts/scriptCabecalho.js
@@ -2,3 +2,7 @@ document.getElementById("login").addEventListener("click", function() {
     console.log("deu");
     document.getElementById("loginTab").style.display = "block";
 });
+
+document.getElementById("closetab").addEventListener("click", function() {
+    document.getElementById("loginTab").style.display = "none";
+});

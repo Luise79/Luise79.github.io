9d6bbcda68774a83f8d39e798435742c40e03e7b
Luise <106396320+Luise79@users.noreply.github.com>
2025-06-16 16:48:30 +0000

    finalizei cabecalho todas as paginas, criei pagina login e finalizei

--- somativa/scripts/scriptCabecalho.js
+++ somativa/scripts/scriptCabecalho.js
@@ -1,12 +1,21 @@
 document.getElementById("login").addEventListener("click", function() {
-    document.getElementById("loginTab").style.display = "block";
+    location.replace("login.html");
 });
 
-document.getElementById("closetab").addEventListener("click", function() {
-    document.getElementById("loginTab").style.display = "none";
-    document.getElementsByClassName("formTab")[0].style.display = "none";
+document.getElementById("loginX").addEventListener("click", function() {
+    location.replace("index.html");
+});
+
+// so esse elemento da null
+document.getElementById("formRecomendadosX").addEventListener("click", function() {
+    location.replace("index.html");
 });
 
 document.getElementById("buttonLogin").addEventListener("click", function() {
     alert("Não é possível fazer o login no momento. Por favor, volte mais tarde!");
+});
+
+document.getElementById("buttonRecomendados").addEventListener("click", function() {
+    console.log(1);
+    
 });
